f8156a91ad913b0d1af3401085df52182f401e57
Subhajit Sahu <wolfram77@gmail.com>
2021-05-31 22:57:11 +0530

    :skunk: w/ L1, L2, Li norms

--- main.cxx
+++ main.cxx
@@ -10,10 +10,12 @@ using namespace std;
 
 template <class G, class H>
 void runPagerank(const G& x, const H& xt, bool show) {
-  int repeat = 5; float damping = 0.85f;
+  int repeat = 5;
+  float damping = 0.85f;
+  int L1 = 1, L2 = 2, Li = 3;
   vector<float> *init = nullptr;
 
-  // Find pagerank using default tolerance 10^-6.
+  // Find pagerank using default tolerance 10^-6, L1-norm.
   auto a1 = pagerankMonolithic(xt, init, {repeat, damping});
   auto e1 = l1Norm(a1.ranks, a1.ranks);
   printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerank\n", a1.time, a1.iterations, e1);
@@ -22,9 +24,21 @@ void runPagerank(const G& x, const H& xt, bool show) {
   // Find pagerank using custom tolerance.
   for (int i=0; i<=20; i++) {
     float tolerance = pow(10.0f, -i/2) / (i&1? 2:1);
-    auto a2 = pagerankMonolithic(xt, init, {repeat, damping, tolerance});
+
+    // Find pagerank using L1 norm for convergence check.
+    auto a2 = pagerankMonolithic(xt, init, {repeat, damping, tolerance, L1});
     auto e2 = l1Norm(a2.ranks, a1.ranks);
-    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerank [tolerance=%.0e]\n", a2.time, a2.iterations, e2, tolerance);
+    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankL1Norm [tolerance=%.0e]\n", a2.time, a2.iterations, e2, tolerance);
+
+    // Find pagerank using L2 norm for convergence check.
+    auto a3 = pagerankMonolithic(xt, init, {repeat, damping, tolerance, L2});
+    auto e3 = l1Norm(a3.ranks, a1.ranks);
+    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankL2Norm [tolerance=%.0e]\n", a3.time, a3.iterations, e3, tolerance);
+
+    // Find pagerank using L∞ norm for convergence check.
+    auto a4 = pagerankMonolithic(xt, init, {repeat, damping, tolerance, Li});
+    auto e4 = l1Norm(a4.ranks, a1.ranks);
+    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankLiNorm [tolerance=%.0e]\n", a4.time, a4.iterations, e4, tolerance);
   }
 }
 
