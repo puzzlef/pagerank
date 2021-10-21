e8eca1dc0aa204c9604a875dc88cb361a8ae81ac
Subhajit Sahu <wolfram77@gmail.com>
2021-10-21 06:26:32 +0530

    :skunk: update src

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-src="pagerank-adjust-damping-factor"
+src="pagerank-adjust-iteration-scaling"
 out="/home/resources/Documents/subhajit/$src.log"
 ulimit -s unlimited
 printf "" > "$out"
